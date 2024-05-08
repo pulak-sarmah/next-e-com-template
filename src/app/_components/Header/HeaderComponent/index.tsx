9420a3c74d1252da2551112fd31925cfedc4892e
pulak-sarmah <pulaksarmah333@gmail.com>
2024-05-08 21:02:25 +0530

    chore: Update Header component to include new HeaderComponent

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,46 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import HeaderMobileNav from '../MobileNav'
+import { HeaderNav } from '../Nav'
+
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link
+          href={'/'}
+          style={{
+            position: 'relative',
+          }}
+        >
+          <Image
+            src="/logo-black.svg"
+            alt="logo"
+            height={50}
+            width={170}
+            priority
+            className={classes.logo}
+          />
+        </Link>
+        <HeaderNav header={header} />
+        <HeaderMobileNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
