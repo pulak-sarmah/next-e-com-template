2411a930f12a98ca45859af71d8cc420c9c33f3a
pulak-sarmah <pulaksarmah333@gmail.com>
2024-05-08 21:01:36 +0530

    chore: Update HeaderNav component to highlight active link

--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -2,9 +2,11 @@
 
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
-import { Header as HeaderType, User } from '../../../../payload/payload-types'
+import { Header as HeaderType } from '../../../../payload/payload-types'
 import { useAuth } from '../../../_providers/Auth'
+import { Button } from '../../Button'
 import { CartLink } from '../../CartLink'
 import { CMSLink } from '../../Link'
 
@@ -13,29 +15,31 @@ import classes from './index.module.scss'
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const navItems = header?.navItems || []
   const { user } = useAuth()
+  const pathname = usePathname()
 
   return (
-    <nav
-      className={[
-        classes.nav,
-        // fade the nav in on user load to avoid flash of content and layout shift
-        // Vercel also does this in their own website header, see https://vercel.com
-        user === undefined && classes.hide,
-      ]
-        .filter(Boolean)
-        .join(' ')}
-    >
+    <nav className={[classes.nav].filter(Boolean).join(' ')}>
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="none" />
+        const isActive =
+          typeof link?.reference.value === 'object' &&
+          link?.reference.value.slug &&
+          pathname.startsWith(`/${link?.reference.value.slug}`)
+        return (
+          <CMSLink
+            key={i}
+            {...link}
+            appearance="none"
+            className={isActive || isActive === null ? classes.activeLink : ''}
+          />
+        )
       })}
-      <CartLink />
-      {user && <Link href="/account">Account</Link>}
-      {!user && (
-        <React.Fragment>
-          <Link href="/login">Login</Link>
-          <Link href="/create-account">Create Account</Link>
-        </React.Fragment>
+      <CartLink className={pathname.startsWith('/cart') ? classes.activeLink : ''} />
+      {user && (
+        <Link href="/account" className={pathname.startsWith('/account') ? classes.activeLink : ''}>
+          Account
+        </Link>
       )}
+      {!user && <Button el="link" href="/login" label="Login" appearance="primary" />}
     </nav>
   )
 }
